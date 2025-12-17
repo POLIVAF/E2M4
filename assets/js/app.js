9187df01ee10d1e01f87cd8ff940f32cc2a6ba3d
POLIVAF <reikikapablo@gmail.com>
2025-12-17 20:53:54 -0300

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+// Objeto que representa a un usuario
+const usuario = {
+  nombre: 'Ana',
+  edad: 24,
+  ciudad: 'Barcelona'
+};
+
+// Arrow function para crear el mensaje de presentación
+const crearMensajePresentacion = (user) => {
+  const {nombre, edad, ciudad} = user;
+  
+
+  const mensaje = `Hola, mi nombre es ${nombre}, tengo ${edad} años y vivo en la ciudad de ${ciudad}.`;
+  
+  return mensaje;
+};
+
+// Se llama a la función y se muestra el resultado en consola
+var mensajeDeBienvenida = crearMensajePresentacion(usuario);
+console.log(mensajeDeBienvenida);
+
+
